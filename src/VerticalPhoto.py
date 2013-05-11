a5713970189ff081d772e271c52a0fe83be3ad69
Shannon Harris <svjharris@gmail.com>
2013-05-11 13:30:18 -0700

    Finished photo child classes

--- src/VerticalPhoto.py
+++ src/VerticalPhoto.py
@@ -1,8 +1,25 @@
-""" PLEASE NOTE: This is currently a psuedoclass and is not meant to be valid code
+""" A class to perform actions on a vertical photo of a face
+    This class inherits from it's parent FacePhoto
 """
 
 class VerticalPhoto(FacePhoto):
 
     __init__(photo):
-        #rotate photo
+        """ Initialize the attributes of a FacePhoto
+
+            This constructor rotates the photo so that we can process
+            it as we would a vertical photo then calls FacePhoto's __init__
+            method to populate the eyes, etc.
+
+            Args:
+              photo photo - a photo of a face
+
+            Return:
+              None
+        """
+        # Rotate photo
+        # NOTE: Not sure if this will rotate the photo to be right side
+        #       up or upside
+        photo.rotate(90)
         # call FacePhoto(super)'s  init
+        super(photo)
