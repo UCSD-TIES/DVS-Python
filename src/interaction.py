49c99f9c2ea42427941759f563d0d32fd2c3227b
jmchan100 <jmchan100@gmail.com>
2014-02-01 13:15:23 -0800

    Commented on everything

--- src/interaction.py
+++ src/interaction.py
@@ -9,23 +9,33 @@ IMGMASK = "JPEG Files(*.jpg;*.jpeg;*.jpe;*.jfif) " \
 
 class interaction():
 	def __init__(self):
-		self.patient = None
-		self.horizontalPath = None
-		self.verticalPath = None
+		self.patient = None          # This might not be used
+		self.horizontalPath = None   # Path of horizontal image
+		self.verticalPath = None     # Path of vertical image
+
+		# These bitmaps are needed to draw on, can't draw on img Ctrls
 		self.hBitMap = None
 		self.vBitMap = None
 
 	# BUTTONS
 
 	# upload button, 1st page
+	# Args: page - the panel the image is on (will be page 1)
+	#       imgCtrl - Either veritcal or horizontal
+	#       text - Text control - either vertical or horizontal
+	#       orientation - specify which image is uploading, 1 for vertical and 0 for horizontal
 	def upload(self, page, imgCtrl, text, orientation):
 	  	# pops up box for user to upload image
 	  	upBox = wx.FileDialog(page, "Choose an image.", os.getcwd(), "",
 							IMGMASK, wx.OPEN)
+
+	  	# Once the user hits the "OK" button
 	  	if upBox.ShowModal() == wx.ID_OK:
-			upPath = upBox.GetPath()
-			text.SetValue(upPath)
-		# 0 for horizontal, 1 for vertical
+			upPath = upBox.GetPath()            # Gets the path of the uploaded file
+			text.SetValue(upPath)               # Sets path into the text control
+
+		# Depending on which image is being uploaded, sets interaction object's
+		# data field and paints the image onto the page.
 	  	if orientation == 0:
 	  		self.upPaint(page, upPath, imgCtrl, orientation)
 	  		self.horizontalPath = upPath
@@ -34,26 +44,45 @@ class interaction():
 	  		self.verticalPath = upPath
 		  
 	# reset button, 1st page
+	# Args: page - panel to clear images and paths (will be page 1)
+	#       imgCtrl1 - vertical or horizontal image control to be cleared
+	#       imgCtrl2 - Other control
+	#       text1 - text control to be cleared
+	#       text2 - other text control to be cleared
 	def reset(self, page, imgCtrl1, imgCtrl2, text1, text2):
 		empty = wx.EmptyImage(440,440)     # Create empty image
+
 		# Sets the 2 image controls to empty
 		imgCtrl1.SetBitmap(wx.BitmapFromImage(empty))
 		imgCtrl2.SetBitmap(wx.BitmapFromImage(empty))
+
 		# Resets path names
 		text1.SetValue("Please upload an image.")
 		text2.SetValue("Please upload an image.")
 		page.Refresh()
 
-	# display the uploaded picture
+		# Deletes interaction object's data 
+		self.horizontalPath = None
+		self.verticalPath = None
+		self.hBitMap = None
+		self.vBitMap = None
+
+	# helper method for upload button, resizes and shows uploaded image
+	# Args: page - the page passed in from upload() (page 1)
+	#       upPath - path of the image to be shown
+	#       imgCtrl - holds the newly shown image
+	#       orientation - vertical or horizontal, taken from upload()
 	def upPaint(self, page, upPath, imgCtrl, orientation):
+		# Makes a new image using the file path
 		newImg = wx.Image(upPath, wx.BITMAP_TYPE_ANY)
 
+		# Getting width and height of this image
 		width = newImg.GetWidth()
 		height = newImg.GetHeight()
 		maxSize = 440
 
 	  # scale the image to preserving the aspect ratio
-		'''
+		'''   UNUSED - FOR NOW
 	  	if width > height and width > maxSize:
 			newWidth = maxSize
 			newHeight = height / (width / float(maxSize))
@@ -73,6 +102,7 @@ class interaction():
 			newHeight = maxSize
 		'''
 
+		# Scales the image
 	  	# This scaling works, but may have problems in future
 	  	if width > height:
 			newWidth = maxSize
@@ -80,9 +110,12 @@ class interaction():
 	  	else:
 		  	newHeight = maxSize
 		  	newWidth = maxSize * width/ height
-		  
+		
+		# Finishes scaling and sets img into the imgCtrl  
 	  	newImg = newImg.Scale(newWidth,newHeight)
 	  	imgCtrl.SetBitmap(wx.BitmapFromImage(newImg))
+
+	  	# Stores data for interaction obj depending on orientation
 	  	if orientation == 0:
 	  		self.hBitMap = wx.BitmapFromImage(newImg)
 	  	elif orientation == 1:
@@ -90,8 +123,14 @@ class interaction():
 	  	page.Refresh()
 
 	# Next Button for first page
+	# Args: hPhotoTxt - text control for horizontal image
+	#       vPhotoTxt - text control for vertical image
+	#       page1 - 1st page
+	#       page2 - 2nd page
+	#       hImgCtrl - horizontal image control of 2nd page
+	#       vImgCtrl - vertical image control of 2nd page
 	def next1(self, hPhotoTxt, vPhotoTxt, page1, page2, hImgCtrl, vImgCtrl):
-		'''
+		'''    These are checks that are turned off for convenience
 		pleaseText = "Please upload an image."
 		if hPhotoTxt == pleaseText and vPhotoTxt == pleaseText:
 			errorTxt1 = "No Images Detected, Please Enter Images"
@@ -113,19 +152,20 @@ class interaction():
 		# Move to next panel
 		else:
 		'''
-		#self.patient = makePatient(self.horizontalPath, self.verticalPath)
+		# Paints the images of page 2 and hides the first page
 		self.upPaint(page2, self.verticalPath, vImgCtrl, 1)
 		self.upPaint(page2, self.horizontalPath, hImgCtrl, 0)
 		page1.Hide()
-		#hImgCtrl.Bind(wx.EVT_PAINT, lambda event: self.OnPaint(hImgCtrl))
+
+		# Drawing rectangle with MemoryDC
 		mdc = wx.MemoryDC()
-		mdc.SelectObject(self.hBitMap)
+		mdc.SelectObject(self.hBitMap)    # sets a bitmap to e modified
 		mdc.SetBrush(wx.Brush('#000000', wx.TRANSPARENT))
 		mdc.DrawRectangle(10, 10, 100, 100)
-		hImgCtrl.SetBitmap(self.hBitMap)
-		mdc.SelectObject(wx.NullBitmap)
-		self.ShowYourself(page2)
-'''
+		hImgCtrl.SetBitmap(self.hBitMap)  # Sets modified bitmap back into the img ctrl
+		mdc.SelectObject(wx.NullBitmap)   # MemoryDC must be set back to a null bitmap when done
+		self.ShowYourself(page2)          # Shows 2nd page
+	'''
 	UNUSED
 	# Painting the Rectangle on the 2nd page
 	def OnPaint(self, control):
@@ -133,18 +173,23 @@ class interaction():
 		draw.Clear()
 		draw.SetBrush(wx.Brush('#000000', wx.TRANSPARENT))
 		draw.DrawRectangle(10, 10, 100, 100)
-'''
-    # The "No" button on page2
+	'''
+    # The "No" button on page2 - Moves from page 2 to 3
+    # Args: page2 - 2nd page
+    #       page3 - 3rd page
+    #       hImgCtrl - 3rd page's horizontal image control
+    #       vImgCtrl - 3rd page's vertical image control
 	def No2(self, page2, page3, hImgCtrl, vImgCtrl):
+		# displays image on 3rd page
 		self.upPaint(page3, self.verticalPath, vImgCtrl, 1)
 		self.upPaint(page3, self.horizontalPath, hImgCtrl, 0)
-		page2.Hide()
-		self.ShowYourself(page3)
+		page2.Hide()                    # Hides 2nd page
+		self.ShowYourself(page3)        # Shows 3rd page
 
 
 
 	# page movement functions
-
+	# Page to be shown
 	def ShowYourself(self, page):
 		page.Raise()
 		page.SetPosition((0,0))
--- src/page.py
+++ src/page.py
@@ -5,61 +5,61 @@ class page(wx.Panel):
 
 	def __init__(self, parent, baseSizer):
 
-		#Image controls and interaction object
-		self.interact = interaction()
-		self.verImgCtrl = None
-		self.horImgCtrl = None
+		self.interact = interaction()   # for button functionality, also holds some data
+		self.verImgCtrl = None          # There's 2 different imgCtrl's in each page
+		self.horImgCtrl = None          # These 2 are just the first page's.
+										# They don't even need to be initialized here
 
 		#page setups
-		self.page1 = wx.Panel(parent)
-		self.pageSetUp(self.page1)
-		baseSizer.Add(self.page1, 1, wx.EXPAND)
+		self.page1 = wx.Panel(parent)   # Creates the page, which is a panel
+		self.pageSetUp(self.page1)      # Each page is set up differently
+		baseSizer.Add(self.page1, 1, wx.EXPAND)   # Add the page to the frame's sizer
 		self.page2 = wx.Panel(parent)
 		self.pageSetUp2(self.page2)
 		baseSizer.Add(self.page2, 1, wx.EXPAND)
 		self.page3 = wx.Panel(parent)
 		self.pageSetUp3(self.page3)
 		baseSizer.Add(self.page3, 1, wx.EXPAND)
-		self.page2.Hide()
+		self.page2.Hide()               # Pages that aren't page 1 start off hidden
 		self.page3.Hide()
 
+	''' Unused
 	def getPage(self, pageNum):
 		if pageNum == 1:
 			return self.page1
 		elif pageNum == 2:
 			return self.page2
+	'''
 
+	''' Moved to interaction.py
 	def ShowYourself(self, page):
 		page.Raise()
 		page.SetPosition((0,0))
 		page.Fit()
 		page.GetParent().GetSizer().Show(page)
 		page.GetParent().GetSizer().Layout()
-
-	# Set up for page 1
+	'''
 	def pageSetUp(self, page):
 		# instantiate the most outer sizer
 		vbox = wx.BoxSizer(wx.VERTICAL)
-		# instantiate the class for interactivity
-
 
 		##############SIZERS#####################
 		# mainGrid has three FlexGrids inside it
 		# wx.FlexGridSizer(rows, cols, vgap, hgap)
-		mainGrid = wx.FlexGridSizer(3, 1, 5, 5)
-		menu = wx.FlexGridSizer(1, 5, 5, 5)
-		pics = wx.FlexGridSizer(1, 2, 5, 5)
-		upload = wx.FlexGridSizer(1, 4, 5, 5)
+		mainGrid = wx.FlexGridSizer(3, 1, 5, 5)    # A column that holds the rest of the sizers
+		menu = wx.FlexGridSizer(1, 5, 5, 5)        # Title, reset, next button
+		pics = wx.FlexGridSizer(1, 2, 5, 5)        # Uploaded photos
+		upload = wx.FlexGridSizer(1, 4, 5, 5)      # Path names and upload buttons
 
 		###############COMPONENTS################
-		verImg = wx.EmptyImage(440,440)
-		self.verImgCtrl = wx.StaticBitmap(page, -1, wx.BitmapFromImage(verImg))
+		verImg = wx.EmptyImage(440,440)            # Images start off as blank images
+		self.verImgCtrl = wx.StaticBitmap(page, -1, wx.BitmapFromImage(verImg))  # Creates it as a bitmap
 		horImg = wx.EmptyImage(440,440)
 		self.horImgCtrl = wx.StaticBitmap(page, -1, wx.BitmapFromImage(horImg))
 
-		title = wx.StaticText(page, label="Welcome to DVS")
+		title = wx.StaticText(page, label="Welcome to DVS")   # Static text for the page
 
-		# Displays path of horizontal image, uneditable
+		# Displays path of images, uneditable
 		horPhotoTxt = wx.TextCtrl(page, size=(350,-1), style=wx.TE_READONLY)
 		horPhotoTxt.SetValue("Please upload an image.")
 		verPhotoTxt = wx.TextCtrl(page, size=(350,-1), style=wx.TE_READONLY)
@@ -67,21 +67,24 @@ class page(wx.Panel):
 
 
 		###################BUTTONS####################
-		nextBtn = wx.Button(page, label='Next')
+		nextBtn = wx.Button(page, label='Next')          # Button for moving onto next page
 		nextBtn.Bind(wx.EVT_BUTTON,
 			lambda event: self.interact.next1(horPhotoTxt.GetValue(), 
 				verPhotoTxt.GetValue(), self.page1, self.page2, self.hor2ImgCtrl, self.ver2ImgCtrl))
+		# Passes in path names for both photos, current page, page 2, creates page 2's imgCtrl's
 
 		# Button to clear pictures and paths
 		resetBtn = wx.Button(page, label='Reset')
 		resetBtn.Bind(wx.EVT_BUTTON,
 			lambda event: self.interact.reset(page, self.horImgCtrl, 
 				self.verImgCtrl, horPhotoTxt, verPhotoTxt))
+		# Passes in current page's imgCtrl's and text controls to delete
 				
 		# Button to upload a horizontal photo
 		horiBtn = wx.Button(page, label='Horizontal')
 		horiBtn.Bind(wx.EVT_BUTTON,
 			lambda event: self.interact.upload(page, self.horImgCtrl, horPhotoTxt, 0))
+		# Passes in the imgCtrl and text control that corresponds to the horizontal image
 
 		# Button to upload a vertical photo
 		vertBtn = wx.Button(page, label='Vertical')
@@ -90,16 +93,17 @@ class page(wx.Panel):
 
 		###################ADDING_STUFF#################
 		# Adding items into the grids
-		mainGrid.AddMany([(menu),(upload),(pics)])
+		mainGrid.AddMany([(menu),(upload),(pics)])         # Adds the three sizers to the main sizer
 
-		menu.AddMany([(title),(595,0),(resetBtn),(nextBtn)])
-		pics.AddMany([(self.horImgCtrl),(self.verImgCtrl)])
-		upload.AddMany([(horPhotoTxt),(horiBtn),(verPhotoTxt),(vertBtn)])
+		# Adds the components to the layout sizers
+		menu.AddMany([(title),(595,0),(resetBtn),(nextBtn)])      # Title, large space, reset, next
+		pics.AddMany([(self.horImgCtrl),(self.verImgCtrl)])       # The two images
+		upload.AddMany([(horPhotoTxt),(horiBtn),(verPhotoTxt),(vertBtn)])   # Text boxes and buttons
 
-		vbox.Add(mainGrid, proportion=1, flag=wx.ALIGN_CENTER|wx.TOP, border=40)
-		page.SetSizer(vbox)
+		vbox.Add(mainGrid, proportion=1, flag=wx.ALIGN_CENTER|wx.TOP, border=40)   # Adds everything to real main sizer
+		page.SetSizer(vbox)                                # Sets the main sizer as the page's sizer
 
-	# Set up for page 2
+	# Set up for page 2 - Most of it is the same as page 1
 	def pageSetUp2(self, page):
 		# instantiate the most outer sizer
 		vbox = wx.BoxSizer(wx.VERTICAL)
@@ -112,19 +116,24 @@ class page(wx.Panel):
 		pics = wx.FlexGridSizer(1, 2, 5, 5)
 
 		###############COMPONENTS################
+		# Note that the imgCtrl's here were made in page 1's next button
 		verImg = wx.EmptyImage(440,440)
 		self.ver2ImgCtrl = wx.StaticBitmap(page, -1, wx.BitmapFromImage(verImg))
 		horImg = wx.EmptyImage(440,440)
 		self.hor2ImgCtrl = wx.StaticBitmap(page, -1, wx.BitmapFromImage(horImg))
 
-		title = wx.StaticText(page, label="Do the boxes frame the eyes?")
+ 
+		title = wx.StaticText(page, label="Do the boxes frame the eyes?")   # Different title from page 1's
 
-		yesBtn = wx.Button(page, label='Yes')
+		yesBtn = wx.Button(page, label='Yes')     # Button to go to 4th page - FUNCTIONALITY NOT ADDED YET
 
-		noBtn = wx.Button(page, label='No')
+		noBtn = wx.Button(page, label='No')       # Button to go to 3rd page
 		noBtn.Bind(wx.EVT_BUTTON,
 			lambda event: self.interact.No2(self.page2, self.page3, self.hor3ImgCtrl, self.ver3ImgCtrl))
+		# Will pass in image controls of 3rd page
 
+		#################ADDING STUFF#################
+		# See page 1's set up
 		mainGrid.AddMany([(menu),(pics)])
 		menu.AddMany([(title),(560,0),(yesBtn),(noBtn)])
 		pics.AddMany([(self.hor2ImgCtrl),(self.ver2ImgCtrl)])
@@ -133,7 +142,8 @@ class page(wx.Panel):
 		vbox.Add(mainGrid, proportion=1, flag=wx.ALIGN_CENTER|wx.TOP, border=40)
 		page.SetSizer(vbox)
 
-	# Set up for page 3
+	# Set up for page 3 - Nothing special about this page yet, 
+	# This is the user input eye detection correction page
 	def pageSetUp3(self, page):
 		# instantiate the most outer sizer
 		vbox = wx.BoxSizer(wx.VERTICAL)
