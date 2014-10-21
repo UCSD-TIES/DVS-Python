ac60b9f7535e6c7f2d67ffdcdb97e28311b8c97c
Gian Costa <giancosta@Gian-Costas-MacBook-Pro.local>
2014-10-21 10:25:00 -0700

    Initial Commit

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ app = wx.App()
 
 #base frame holds everything
 #wx.Frame(parent, id=-1, title=EmptyString, pos=DefaultPosition,
-#	size=DefaultSize, style=DEFAULT_FRAME_STYLE, name=FrameNameStr)
+#	 size=DefaultSize, style=DEFAULT_FRAME_STYLE, name=FrameNameStr)
 base(None, -1, 'Digital Vision Screening',
 	pos = wx.DefaultPosition,
 	size = (1000,600),
